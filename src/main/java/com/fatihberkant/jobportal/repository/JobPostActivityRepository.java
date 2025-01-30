c4d36fcecd6620f77cf425f4adcccb0618caa1ca
Fatih Berkant EREN <fatihberkant38@gmail.com>
2025-01-30 11:15:44 +0300

    Recruiter - Post New Job

---
+++ src/main/java/com/fatihberkant/jobportal/repository/JobPostActivityRepository.java
@@ -0,0 +1,7 @@
+package com.fatihberkant.jobportal.repository;
+
+import com.fatihberkant.jobportal.entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+}
---
+++ src/main/java/com/fatihberkant/jobportal/services/JobPostActivityService.java
@@ -0,0 +1,21 @@
+package com.fatihberkant.jobportal.services;
+
+import com.fatihberkant.jobportal.entity.JobPostActivity;
+import com.fatihberkant.jobportal.repository.JobPostActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobPostActivityService {
+
+    private final JobPostActivityRepository jobPostActivityRepository;
+
+    @Autowired
+    public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
+        this.jobPostActivityRepository = jobPostActivityRepository;
+    }
+
+    public JobPostActivity addNew(JobPostActivity jobPostActivity) {
+        return jobPostActivityRepository.save(jobPostActivity);
+    }
+}
